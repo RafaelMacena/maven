83051b8884621ce5252ee6383122f880f56c4525
RafaelMacena <rafaelsouzasouza1945@gmail.com>
2022-11-17 12:24:15 -0300

    estudo build e depedências com maven

---
+++ src/main/java/br/com/rafaelmacena/loja/Produto.java
@@ -0,0 +1,5 @@
+package br.com.rafaelmacena.loja;
+
+public class Produto {
+
+}
---
+++ src/test/java/br/com/rafaelmacena/loja/ProdutoTest.java
@@ -0,0 +1,11 @@
+package br.com.rafaelmacena.loja;
+
+import org.junit.Test;
+
+public class ProdutoTest {
+
+	@Test
+	public void test() {
+	}
+
+}
